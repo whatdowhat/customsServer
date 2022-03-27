cf7f1fa08c94362bd982e803c8fee34e6d662dbc
whatdowhat <rhengh01@gmail.com>
2022-03-28 00:13:58 +0900

    충돌수정

--- webCustoms/src/main/java/com/keepgo/whatdo/controller/User/UserController.java
+++ webCustoms/src/main/java/com/keepgo/whatdo/controller/User/UserController.java
@@ -2,47 +2,25 @@ package com.keepgo.whatdo.controller.User;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.expression.ParseException;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 
-import com.keepgo.whatdo.controller.CompanySpecification;
-import com.keepgo.whatdo.entity.customs.Common;
-import com.keepgo.whatdo.entity.customs.CompanyInfo;
-import com.keepgo.whatdo.entity.customs.CompanyInfoExport;
-import com.keepgo.whatdo.entity.customs.CompanyInfoManage;
-import com.keepgo.whatdo.entity.customs.request.CommonReq;
-import com.keepgo.whatdo.entity.customs.request.CommonReqForExcelDownload;
-import com.keepgo.whatdo.entity.customs.request.CompanyInfoReq;
 import com.keepgo.whatdo.entity.customs.request.UserReq;
-import com.keepgo.whatdo.entity.customs.response.CommonRes;
-import com.keepgo.whatdo.entity.customs.response.CompanyInfoRes;
 import com.keepgo.whatdo.entity.customs.response.UserRes;
 import com.keepgo.whatdo.mapper.UserMapper;
 import com.keepgo.whatdo.repository.CompanyInfoExportRepository;
@@ -54,7 +32,6 @@ import com.keepgo.whatdo.service.user.UserService;
 import com.keepgo.whatdo.util.CustomExcel;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000",allowCredentials = "true" ) // 컨트롤러에서 설정
 public class UserController {
 
 	static final Logger log = LoggerFactory.getLogger(UserController.class);
@@ -86,8 +63,6 @@ public class UserController {
 		
 
 		return  _userService.getAll(userReq);
-
-		
 	}
 	
 	@RequestMapping(value = "/user/addData", method = {RequestMethod.POST })

a105674e0a61c25bba04cc6d1dab162f4c23954d
whatdowhat <rhengh01@gmail.com>
2022-04-28 20:09:21 +0900

    server

---
+++ webCustoms/src/main/java/com/keepgo/whatdo/entity/customs/response/InboundViewListRes.java
@@ -0,0 +1,49 @@
+package com.keepgo.whatdo.entity.customs.response;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.keepgo.whatdo.entity.customs.Common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class InboundViewListRes {
+
+	
+		
+	private String itemCountSumFinal;
+	private String boxCountSumFinal;
+	private String cbmSumFinal;
+	private String weightSumFinal;
+	
+	
+	private List<InboundViewRes> inbounds = new ArrayList();
+	
+	
+	
+}
